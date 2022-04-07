20c596f17b6c8f6f17d6f0d669cc0ffb09d12f2f
German Frate <frate_g@solarsistemas.net>
2022-04-07 11:20:35 -0300

    incorporate table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import { Typography } from 'antd';
-const { Text } = Typography;
+import { useEffect, useState } from 'react';
+import { Table, Typography } from 'antd';
+const { Text, Title } = Typography;
 
 function App() {
-  const [starships, setStarships] = useState('Naves');
-
+  const [datasource, setDatasource] = useState();
   async function fetchMovies() {
     let naves = await fetch('https://swapi.dev/api/starships/');
     let respuesta = await naves.json();
-    console.log(respuesta);
-    setStarships(respuesta.results.map(ship => ship.name));
+    let allships = respuesta.results.reduce((ships, ship, index) =>
+      ships.concat({
+        key: `${index}`,
+        name: `${ship.name}`,
+        model: `${ship.model}`,
+        manufacturer: `${ship.manufacturer}`
+      }), []
+    );
+    console.log("array de naves", allships);
+    setDatasource(allships);
   };
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name'
+    },
+    {
+      title: 'Model',
+      dataIndex: 'model',
+      key: 'model'
+    },
+    {
+      title: 'Manofacturer',
+      dataIndex: 'manufacturer',
+      key: 'manufacturer'
+    }
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => fetchMovies()}>Fetch Movies</button>
-        <Text type="danger">{starships}</Text>
+        <Title leve={2}>Spaceships</Title>
+        <button onClick={() => fetchMovies()}>Fetch Ships</button>
+        {datasource ? <Table dataSource={datasource} columns={columns} pagination={false} /> : <Text>Not ready</Text>}
       </header>
     </div>
   );
